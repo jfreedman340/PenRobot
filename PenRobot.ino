ba7316a267bfb0efc1f5663296f96c8979081a96
jfreedman340 <97777103+jfreedman340@users.noreply.github.com>
2022-11-13 00:27:48 -0800

    Simpified it more
    I reduced the functions down to one function named axisMove(); and it has parameters for the direct pin, digital output value, step pin, and the amount of steps. Based on the axisMove(); function I created a xMove(); and yMove(); function with parameters for the digital outputs and I am using them in a function called sixPasses(); which does what the name implies, the machine makes six passes. I may or may not need six passes in production, I might be able to reduce it to three especially when I get a diamond blade for it.
    
    The next step will be to create a curvedCuts(); function that will be called after sixPasses(); and it will be the final cutting pass which will be slightly curved cuts on the pen blanks to give them a nice curvature and make the sanding much faster.
    
    This code is way better than my previous code and it works great so far, however there's probably a much better way to do this and I look forward to finding out!
    
    Live long and prosper,
    Joseph Freedman

--- PenRobot.ino
+++ PenRobot.ino
@@ -1,11 +1,11 @@
-// Define stepper motor connections and steps per revolution:
+// Define stepper motor connections and steps per rev:
 #define dirX 2
 #define stepX 3
 
 #define dirY 4
 #define stepY 5
 
-#define stepsPerRevolution 1600
+#define stepsPerRev 1600
 
 void setup() {
   // Declare pins as output:
@@ -19,7 +19,7 @@ void axisMove (uint8_t function_dirPin, uint8_t value, int stepPin, int steps) {
   // Set the spinning direction counter-clockwise:
   digitalWrite(function_dirPin, value);
 
-  // Spin the stepper motor 1 revolution quickly:
+  // Spin the stepper motor 1 rev quickly:
   for (int i = 0; i < steps; i++) {
     // These four lines result in 1 step:
     digitalWrite(stepPin, HIGH);
@@ -30,54 +30,28 @@ void axisMove (uint8_t function_dirPin, uint8_t value, int stepPin, int steps) {
 }
 
 
+void xMove (uint8_t value) {axisMove(dirX, value, stepX, stepsPerRev * 10);}
+void yMove (uint8_t value, int steps) {axisMove(dirY, value, stepY, steps);}
+
+void sixPasses () {
+  //Pass 1
+  xMove(LOW), yMove(LOW, stepsPerRev),
+  //Pass 2
+  xMove(HIGH), yMove(LOW, stepsPerRev),
+  //Pass 3
+  xMove(LOW), yMove(LOW, stepsPerRev),
+  //Pass 4
+  xMove(HIGH), yMove(LOW, stepsPerRev),
+  //Pass 5
+  xMove(LOW), yMove(LOW, stepsPerRev),
+  //Pass 6
+  xMove(HIGH), yMove(HIGH, stepsPerRev * 5);
+  }
+
 
 void loop() {
 
-  // Make a pass to the right by 20 stepsPerRevolution
-  axisMove(dirX, LOW, stepX, stepsPerRevolution * 20);
-
-
-  // Move blade forward
-  axisMove(dirY, LOW, stepY, stepsPerRevolution/2);
-  
-
-  // Make a pass to the left by 20 stepsPerRevolution
-  axisMove(dirX, HIGH, stepX, stepsPerRevolution * 20);
-
-
-  // Move blade forward second time
-  axisMove(dirY, LOW, stepY, stepsPerRevolution/2);
-
-
-  // Make a pass to the right by 20 stepsPerRevolution
-  axisMove(dirX, LOW, stepX, stepsPerRevolution * 20);
-
-
-  // Move blade forward third time
-  axisMove(dirY, LOW, stepY, stepsPerRevolution/2);
-
-
-  // Make a pass to the left by 20 stepsPerRevolution
-  axisMove(dirX, HIGH, stepX, stepsPerRevolution * 20);
-
-
-  // Move blade forward fourth time
-  axisMove(dirY, LOW, stepY, stepsPerRevolution/2);
-
-
-  // Make a pass to the right by 20 stepsPerRevolution
-  axisMove(dirX, LOW, stepX, stepsPerRevolution * 20);
-
-
-  // Move blade forward fifth time
-  axisMove(dirY, LOW, stepY, stepsPerRevolution/2);
-  
-
-  // Make a pass to the left by 20 stepsPerRevolution
-  axisMove(dirX, HIGH, stepX, stepsPerRevolution * 20);
-  
-
-  // Move blade back to starting position
-  axisMove(dirY, HIGH, stepY, 4000);
+  // Make a six passes
+  sixPasses();
 
 }
