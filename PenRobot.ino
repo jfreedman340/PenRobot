ccdea4234e86d7acafc28a9193c08a68c89686f5
jfreedman340 <97777103+jfreedman340@users.noreply.github.com>
2022-11-12 17:09:11 -0800

    Add files via upload

---
+++ PenRobot.ino
@@ -0,0 +1,23 @@
+#define dirPin 2
+#define stepPin 3
+#define stepsPerRevolution 1600
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  
+  class PenRobot {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    
+};
+int main() {
+  PenRobot myObj;  // Create an object of MyClass
+}
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
